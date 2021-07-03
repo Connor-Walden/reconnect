e54a11a3831aa0e050b42e68b9c053c356440859
Connor Walden <connor.walden@lumary.com.au>
2021-07-03 11:35:08 +0930

    Updated project to use typescript

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,19 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+interface props {
+  children: React.ReactNode
+}
+
+
+const Layout: React.FC<props> = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -46,10 +51,6 @@ const Layout = ({ children }) => {
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
